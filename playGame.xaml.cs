a3d419d394cc65054f541fae2608882f3b3c7ee0
Gabriel E. Rodriguez Garcia <gr.garcia1121@gmail.com>
2023-07-02 20:18:23 -0400

    Finished joining playGame Xaml form and playGame C# code.

--- playGame.xaml.cs
+++ playGame.xaml.cs
@@ -1,12 +1,25 @@
 namespace Treedle;
-
+/**
+ * TO-DO: Work on title banner so i can fully remove the Xmal form.
+ */
 public partial class playGame : ContentPage
 {
 	public playGame()
 	{
 		InitializeComponent();
-		//createGrid();
-        //createKeyboard();
+
+        //Creates the verticle layout that holds the items in the page
+        VerticalStackLayout verticalStackLayout = new VerticalStackLayout { };
+
+        //Adds the game grid to the vertical layout
+       verticalStackLayout.Add(createGameGrid());
+
+        //Adds all three rows of the keyboard to the vertical layout
+        for(int keyboardRow = 0; keyboardRow < 3; keyboardRow++)
+            verticalStackLayout.Add(createKeyboardGrid(keyboardRow));
+
+        //Adds the vertical layout contents to the Content Page
+        Content = verticalStackLayout;
 
 
 	}
@@ -14,16 +27,14 @@ public partial class playGame : ContentPage
      * Creates the grid system in treedle. 
      * Creates all 6 rows and 5 Columns 
      */
-	public void createGrid()
+	public Grid createGameGrid()
 	{
-        //Creates the verticle layout that holds the items in the page
-        VerticalStackLayout verticalStackLayout = new VerticalStackLayout{};
 
         //Creates the grid with the appropriate cosmetics and settings
         Grid grid = new Grid
         {
             BackgroundColor = Colors.White,
-            Margin = new Thickness(3),
+            Margin = new Thickness(0,20,0,200),
             ColumnSpacing = 10,
             RowSpacing = 10,
             HorizontalOptions = LayoutOptions.Center,
@@ -53,11 +64,7 @@ public partial class playGame : ContentPage
             for( int col = 0; col < 6; col++ ) 
                 grid.Add(createBorder(createLabel()),row,col);
 
-        //Adds the grid to the Vertical Layout
-        verticalStackLayout.Add(grid);
-
-        // Adds content to page
-        Content = verticalStackLayout;
+        return grid;
         
     }
 
@@ -90,41 +97,77 @@ public partial class playGame : ContentPage
         {
             Content = label,
             StrokeThickness = 3,
-            Padding = new Thickness(20,5),
+            Padding = new Thickness(20, 5),
             HorizontalOptions = LayoutOptions.Center,
-            
+
         };
     }
-
-    public void createKeyboard()
+    /**
+     * Creates the keyboard grid.
+     * Receives the row number of the keyboard. 
+     * Returns the grid of buttons for the assigned row
+     */
+    public Grid createKeyboardGrid(int rowNumber)
     {
-        VerticalStackLayout verticalStackLayout = new VerticalStackLayout{ };
-
-        Grid grid = new Grid
-        {
+        //Keyboard design 
+        Grid grid = new Grid {
             BackgroundColor = Colors.DarkGray,
+            ColumnSpacing = 10,
+            RowSpacing = 10,
+            Margin = new Thickness(3),
+            Padding = new Thickness(35, 0, 35, 0),
+            HorizontalOptions = LayoutOptions.Center,
             RowDefinitions =
             {
-                new RowDefinition(),
-                new RowDefinition(),
                 new RowDefinition()
-               
+
             },
             ColumnDefinitions =
             {
-                //10,9,10
                 new ColumnDefinition(),
                 new ColumnDefinition(),
-                new ColumnDefinition()
+                new ColumnDefinition(),
+                new ColumnDefinition(),
+                new ColumnDefinition(),
+                new ColumnDefinition(),
+                new ColumnDefinition(),
+                new ColumnDefinition(),
+                new ColumnDefinition(),
+                new ColumnDefinition(),
             }
         };
+
+        //List of List to create the row of letters of the keyboard.
+        List<List<string>> myList = new List<List<string>>();
+        myList.Add(new List<string> {"Q", "W", "E" , "R","T" ,"Y" ,"U" ,"I" ,"O" ,"P"});
+        myList.Add(new List<string> { "A", "S", "D", "F", "G", "H", "J", "K", "L" });
+        myList.Add(new List<string> { "Enter", "Z", "X", "C", "V", "B", "N", "M", "Delete" });
+
+        int col = 0;
+        foreach(string item in myList[rowNumber])
+            grid.Add(createButton(item), col++, rowNumber);
+        
+
+
+        return grid;
     }
-
-    public Button createButton()
+    /**
+     * Creates a keyboard button. 
+     * Receives the keyboard letter
+     * Returns the button with the designated cosmetics and settings
+     */
+    public Button createButton(string letter)
     {
-        return new Button
-        {
-
-        };
+        if(letter.CompareTo("Enter") == 0 || letter.CompareTo("Delete") == 0)
+            return new Button {
+                Text = letter,
+                WidthRequest= 70,
+                BackgroundColor = Colors.Gray
+            };
+        else
+            return new Button {
+                Text = letter,
+                BackgroundColor = Colors.Gray
+            };
     }
 }
