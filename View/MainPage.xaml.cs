0525fa57465a0db4474bc9ef1452d08d2bf2aee4
Gabriel E. Rodriguez Garcia <gr.garcia1121@gmail.com>
2023-08-09 15:14:59 -0400

    Commented all view classes

--- View/MainPage.xaml.cs
+++ View/MainPage.xaml.cs
@@ -1,52 +1,51 @@
-﻿using Mopups.Services;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using Treedle.Model;
 using Treedle.Service;
 using Treedle.View;
-using Treedle.ViewModel;
 
 namespace Treedle;
-
+/**
+ * Main Page View Class
+ */
 public partial class MainPage : ContentPage
 {
-
+    //Creates instance of word service
     WordService wordService;
 
+    //TEmporary list to store word objects
     public ObservableCollection<Word> OriginalList { get; } = new();
 
+    //List to store all string versions of the WordList
     public List<string> WordList { get; } = new();
 
     public MainPage()
 	{
+        //Sets instance of Wordservice class to new WordSevice
         wordService = new WordService();
 
+        //Runs asynchronously the task to fill the world list
         Task.Run(fillWordList);
 
 		InitializeComponent();
 	}
-
+    /**
+     * Button to start the game
+     */
     private async void btn_play_Clicked(object sender, EventArgs e)
     {
+        //If the word list is empty dont allow the button to be executed.
         if(WordList.Count > 0) 
             await Navigation.PushAsync(new GamePage(WordList));
     }
-
+    /**
+     * Task to fill the word list using an instance w=of Word Service
+     */
     public async Task fillWordList()
     {
         var jsonlist = await wordService.GetWords();
 
         foreach (var word in jsonlist)
             WordList.Add(word.word);
-
-    }
-
-    private void login_Clicked(object sender, EventArgs e)
-    {
-
-    }
-
-    private void how_Clicked(object sender, EventArgs e)
-    {
 
     }
 }
--- View/NotWordPage.xaml.cs
+++ View/NotWordPage.xaml.cs
@@ -1,5 +1,8 @@
 namespace Treedle.View;
-
+/**
+ * Creates an instance of a pop up message to tell the user that the word is not in the sentence
+ * 
+ */
 public partial class NotWordPage
 {
 	public NotWordPage()
